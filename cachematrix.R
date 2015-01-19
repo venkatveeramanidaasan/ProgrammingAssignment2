100af80befeaa81bdd0aa33e32d2ab050639cdcf
Venkata Krishnan V <ve_krish@yahoo.com>
2015-01-19 08:55:26 +0530

    Implemented assignment 2. To cache the inversed matrix using solve and lexical scoping techniques.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,51 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mdata = matrix()) {
+    cached_solve <- NULL
+    
+    set <- function(ndata) {
+        mdata <<- ndata
+        cached_solve <<- NULL
+    }
+    
+    get <- function() {
+        mdata
+    } 
+    
+    setsolve <- function(inv_matrix) {
+        cached_solve <<- inv_matrix
+    }
+    
+    getsolve <- function() {
+        cached_solve
+    } 
+    
+    list(set = set, get = get, 
+         setsolve = setsolve, 
+         getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mcm, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    mi <- mcm$getsolve()
+            
+    if (!is.null(mi)) {
+        message("Catch hit")
+        return(mi)
+    }
+    
+    message("Cache miss")
+    
+    cdata <- mcm$get()
+    
+    message(cdata)
+    mi <- solve(cdata, ...)
+    
+    mcm$setsolve(mi)
+    
+    mi 
 }
