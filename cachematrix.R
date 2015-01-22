81fa3de8724843a79e4196061b9a3cd06b805441
Venkata Krishnan V <ve_krish@yahoo.com>
2015-01-22 18:57:19 +0530

    Updated code with comments and test output

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,58 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Matrix inversion on big matrix data is time consuming. 
+## Caching the inversed matrix using lexical scoping techniques.
+## Helps us to avoid from making a call to sovle function everytime we need the 
+## same inverse data.
+# Test Output:
 
+## mat <- matrix(1:4, 2, 2)
+
+## mcm <- makeCacheMatrix(mat)
+## cacheSolve(mcm)
+# Cache miss
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+
+#Here we have reset the function. So, no cache
+# mcm <- makeCacheMatrix(mat) 
+# cacheSolve(mcm)
+# Cache miss
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+
+# cacheSolve(mcm)
+# Catch hit
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+
+# solve(cacheSolve(mcm))
+# Catch hit
+# [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+
+## The makeCacheMatrix takes matrix data as its input. 
+## By default it takes empty one. 
+## It helps us maintaining the cached inversed matrix data.
+## It returns the list of functions, Which will be consume by makeSolve function.
+## function is a first class object in R. 
+## So, we could pass function object to another function.
+## The inner function takes the advantage of lexical scoping to read or write data
+## into the varialbes declared outside its scope/parent environment using <<- operator
 makeCacheMatrix <- function(mdata = matrix()) {
     cached_solve <- NULL
     
     set <- function(ndata) {
         mdata <<- ndata
+        ## Whenever data changes. We have to invalidate it. 
+        ## So, the next call to getSolve will return NULL.
+        ## Then, the makeSolve will invoke on solve on new matrix data and 
+        ## caches it using setsolve()
         cached_solve <<- NULL
     }
     
@@ -29,7 +74,7 @@ makeCacheMatrix <- function(mdata = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function takes the list returned by makeCacheMatrix function.
 
 cacheSolve <- function(mcm, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -44,9 +89,10 @@ cacheSolve <- function(mcm, ...) {
     
     cdata <- mcm$get()
     
-    message(cdata)
+    ## Inserve the matrix
     mi <- solve(cdata, ...)
     
+    ## Cache it
     mcm$setsolve(mi)
     
     mi 
